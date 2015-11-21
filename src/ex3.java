5ab64d741e145c3dd0643d867e72b029dc0d2059
James Lett <jlett89@gmail.com>
2015-11-21 15:58:42 -0800

    Just some java practice

---
+++ src/ex3.java
@@ -0,0 +1,21 @@
+
+public class ex3 {
+    public static void main(String[] args) {
+        int myInt = 7;
+
+        String text = "Hello";
+
+        String blank = " ";
+
+        String name = "James";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+
+        System.out.println("Hello" + " " + "James");
+
+        System.out.println("My integer is " + myInt);
+
+    }
+}
